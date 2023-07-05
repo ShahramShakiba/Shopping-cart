93ea601a6efc141cbfa3c2cd6e2d701dca9db070
Shahram-Shakiba <110089830+ShahramShakiba@users.noreply.github.com>
2023-07-06 00:17:13 +0430

    getCart & update cartValue

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ setInterval(() => {
 }, 1000);
 
 /*==================== ProductsData ===================*/
-// import productsData
+//-> import productsData
 import { productsData } from './Products.js';
 
 /*======================== Modal ======================*/
@@ -28,14 +28,18 @@ backDrop.addEventListener('click', toggleModal);
 
 /*==================== Get Products ===================*/
 class Products {
-  // while loading the page get products from api end point
+  //-> while loading the page get products from api end point
   getProducts() {
     return productsData;
   }
 }
 
 /*================== Display Products =================*/
-const productsDOM = document.querySelector('.products-center');
+const productsDOM = document.querySelector('.products-center'),
+  cartTotal = document.querySelector('.cart-total'),
+  cartItemsCount = document.querySelector('.cart-items');
+
+let cart = [];
 
 class UI {
   displayProducts(products) {
@@ -70,12 +74,76 @@ class UI {
 
     productsDOM.innerHTML = result;
   }
+
+  getCartBtns() {
+    const addCartBtn = document.querySelectorAll('.add-to-cart');
+
+    //-> convert NodeList to Array
+    const buttons = [...addCartBtn];
+
+    buttons.forEach((btn) => {
+      const id = btn.dataset.id;
+
+      //-> check if product is in the cart
+      const isExist = cart.find((p) => p.id === id);
+      if (isExist) {
+        btn.textContent = 'Added';
+        btn.disabled = true;
+      }
+
+      btn.addEventListener('click', (e) => {
+        //-> when btn clicked to add product to cart
+        e.target.textContent = 'Added';
+        e.target.disabled = true;
+
+        //-> get products from localStorage
+        const addedProduct = Storage.getProduct(id);
+        console.log(addedProduct);
+        //-> add to cart
+        cart = [...cart, { ...addedProduct, quantity: 1 }];
+
+        //-> save cart to localStorage
+        Storage.saveCart(cart);
+
+        //-> update cart value
+        this.setCartValue(cart);
+
+        //-> get cart from localStorage to avoid resetting the cart value 
+      });
+    });
+  }
+
+  setCartValue(cart) {
+    //-> total price of cart
+    let tempCartItems = 0;
+
+    const totalPrice = cart.reduce((acc, curr) => {
+      //-> show number of items in cart
+      tempCartItems += curr.quantity;
+
+      return acc + curr.quantity * curr.price;
+    }, 0);
+
+    cartTotal.textContent = `Total price: $ ${totalPrice.toFixed(2)}`;
+    cartItemsCount.textContent = tempCartItems;
+  }
 }
 
 /*==================== localStorage ===================*/
 class Storage {
   static saveProducts(products) {
     localStorage.setItem('products', JSON.stringify(products));
+  }
+
+  static getProduct(id) {
+    const _products = JSON.parse(localStorage.getItem('products'));
+
+    // parseInt() -> covert string to integer
+    return _products.find((p) => p.id === parseInt(id));
+  }
+
+  static saveCart(cart) {
+    localStorage.setItem('cart', JSON.stringify(cart));
   }
 }
 
@@ -86,6 +154,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const ui = new UI();
   ui.displayProducts(productsData);
+  ui.getCartBtns();
 
   Storage.saveProducts(productsData);
 });
